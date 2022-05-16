1263264a6f13d9236071178bb587a780634916e2
Fernando Moyano <nanomoyano@gmail.com>
2022-05-16 16:48:07 +0200

    analysis code

---
+++ piao2008_get_ccgcrv.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def get_ccgcrv(co2_file='../data/piao_2008/piao_co2_fm.csv', 
+    stations_file='../data/piao_2008/piao_stations.csv',
+    start_col = 'start_piao2008', end_col = 'end_piao2008'):
+    # Function takes a list of stations and respective co2 time series and outputs ccgcrv fitted data.
+    # ccgcrv module function is used and results saved in a dictionary, with keys being station names
+    # co2_file: file with the co2 data
+    # stations_file: file with station summary data
+    # start_col: column in stations_file with starting year
+    # end_col: column in stations_file with ending year
+
+    from fun_ccg_fits import ccg_fits
+    import pandas as pd
+    from copy import deepcopy
+    import numpy as np
+
+    # Read in data
+    co2_fm = pd.read_csv(co2_file)
+    stations = pd.read_csv(stations_file)
+
+    # Default parameter values as used in the ccgcrv code.
+    ccg_pars_def = {
+        'shortterm': 80,
+        'longterm': 667,
+        'sampleinterval': 0,
+        'numpolyterms': 3,
+        'numharmonics': 4,
+        'timezero': -1,
+        'gap': 0,
+        'use_gain_factor': False,
+        'debug': False
+    }
+
+    # Set sampleinterval to 1
+    ccg_pars1 = deepcopy(ccg_pars_def)
+    ccg_pars1['sampleinterval'] = 1
+
+    # Get ccgcrv fits for each station and save to a dictionary
+    ccg_output = dict()
+
+    for s in stations['name'].unique():
+        ystart = int(stations.loc[stations['name'] == s, start_col])
+        yend = int(stations.loc[stations['name'] == s, end_col])
+        sel = (co2_fm['station'] == s) & (co2_fm['year'].between(ystart, yend, inclusive='both'))
+        input = co2_fm.loc[sel, ['co2', 'dec_year']]
+        out = ccg_fits(data=input, pars=ccg_pars1)
+        ccg_output[s] = out
+
+
+    # Create dataframe with stations and results ----
+
+    # Here could remove bad years e.g. by using the Gini coefficient as in Forkel 2016
+
+    ccgmeans = deepcopy(pd.DataFrame(stations['name']))
+    ccgmeans['amp_mean'] = np.nan
+    ccgmeans['min_mean'] = np.nan
+    ccgmeans['max_mean'] = np.nan
+    ccgmeans['szc_mean'] = np.nan
+    ccgmeans['azc_mean'] = np.nan
+
+    for s in stations['name'].unique():
+        res = ccg_output[s]
+        amp_mean = res['yearly']['amp'].mean()
+        min_mean = res['yearly']['min_value'].mean()
+        max_mean = res['yearly']['max_value'].mean()
+        szc_mean = res['yearly']['tcd_doy'].mean()
+        azc_mean = res['yearly']['tcu_doy'].mean()
+        ccgmeans.loc[ccgmeans['name']==s, 'amp_mean'] = np.round(amp_mean, 2)
+        ccgmeans.loc[ccgmeans['name']==s, 'min_mean'] = np.round(min_mean, 2)
+        ccgmeans.loc[ccgmeans['name']==s, 'max_mean'] = np.round(max_mean, 2)
+        ccgmeans.loc[ccgmeans['name']==s, 'szc_mean'] = int(szc_mean)
+        ccgmeans.loc[ccgmeans['name']==s, 'azc_mean'] = int(azc_mean)
+
+    return ccg_output, ccgmeans
---
+++ piao2008_get_tmp_mean.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def get_tmp_mean(
+    tmp_file='/Users/moyanofe/BigData/GeoSpatial/Climate/CRU-TS_4.05_1901-2020/cru_ts4.05.1901.2020.tmp.dat.nc',
+    limS=30,
+    limN=80):
+    # Function calculates averages for yearly and seasonally for region of interest saves to file.
+    # limS: southernmost latidude to determine region of interest
+    # limN: northernmost latidude to determine region of interest
+
+
+    # Load modules ----
+
+    import numpy as np
+    import pandas as pd
+    import xarray as xr
+
+
+    # Load the data adn select region ----
+
+    ds_t = xr.open_dataset(tmp_file)
+    da_t = ds_t['tmp']
+    da_tr = da_t.sel(lat=slice(limS,limN)) # Get values only for the region of interest
+
+
+    # Get yearly averages for the region ----
+
+    # See https://docs.xarray.dev/en/stable/examples/area_weighted_temperature.html
+
+    # First average in time
+    da_try = da_tr.groupby('time.year').mean() # try = temperature, region, yearly
+    # Then average for the region (weight by gridcell size)
+    weights = np.cos(np.deg2rad(da_try.lat)) # cosine of latitude is a proxy of gridcell size
+    weights.name = "weights" # Not sure why this is required 
+    da_tryw = da_try.weighted(weights) # Get weighted values
+    da_trym = da_tryw.mean(('lat', 'lon')) # Get the average over lat and lon
+
+
+    # Get seasonal averages ----
+
+    # See: https://docs.xarray.dev/en/stable/examples/monthly-means.html
+
+    # To be precise, need to weight by the actual month lengths
+
+    # First create a year_season coordinate used to group the data
+    year_np = da_tr['time.year'].to_pandas()
+    season_np = da_tr['time.season'].to_pandas()
+    year_season_idx = pd.MultiIndex.from_arrays([year_np, season_np])
+    da_tr.coords['year_season'] = ('time', year_season_idx)
+
+    # Create a data array with number of days per month
+    month_length = da_tr.time.dt.days_in_month
+
+    # Calculate the weights for each month by grouping by 'year_season'.
+    weights =  month_length.groupby("year_season") / month_length.groupby("year_season").sum()
+
+    # Test that the sum of the weights for each season is 1.0. (No output or None is good)
+    np.testing.assert_allclose(weights.groupby("year_season").sum().values, np.ones(480))
+
+    # Calculate the weighted average for each season and year (i.e. the sum of the weighted values)
+    da_trs = (da_tr * weights).groupby("year_season").sum(dim="time")
+
+    # Get the average for the entire region (weight by gridcell size)
+    weights = np.cos(np.deg2rad(da_trs.lat)) # cosine of latitude is a proxy of gridcell size
+    weights.name = "weights"
+    da_trsw = da_trs.weighted(weights) # Get weighted values
+    da_trsm = da_trsw.mean(('lat', 'lon')) # Get the average over lat and lon
+
+
+    # Convert to pandas df ----
+
+    # First get the year-season coordinate and convert to a pandas MultiIndex
+    ys = da_trsm['year_season'].to_pandas()
+    index = pd.MultiIndex.from_tuples(ys)
+    # Second convert arraydata and MultiIndex to dataframe (the latter maybe unnecessary)
+    df1 = da_trsm.to_dataframe(name='tmp')
+    df2 = index.to_frame()
+    # Third concat the dfs and add the index
+    ts = pd.concat([df1, df2], axis=1)
+    ts = ts.set_index(index)
+    ts.columns = ['tmp', 'year', 'season']
+
+
+    # Add to the yearly dataframe ----
+
+    tm = da_trym.to_dataframe(name='tmp_year')
+
+    for s in ['DJF', 'MAM', 'JJA', 'SON']:
+        name = 'tmp_' + s
+        data = ts.loc[ts['season']==s, 'tmp']
+        data = data.reset_index(level=1, drop=True)
+        tm[name] = data
+
+
+    # Save to file ----
+
+    file_out = 'tmp_yearly_means_' + str(limS) + str(limN) + 'N.csv'
+    tm.to_csv('../data/piao_2008/' + file_out)
+
+    return tm
